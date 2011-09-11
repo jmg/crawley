3aa846860a4a48ae148af4d8d16898ae56716aaa
Juan Manuel García <jmg.utn@gmail.com>
2011-09-11 14:40:22 -0300

    Decorator enhacenments and syncb command

--- crawley/manager/commands.py
+++ crawley/manager/commands.py
@@ -13,23 +13,34 @@ commands = {}
 def command(f):
     
     commands[f.__name__] = f
+    
+    def decorated(*args, **kwargs):
+        f(*args, **kwargs)
+    
+    return decorated
 
+
+@command 
+def syncdb(settings):
+    
+    elixir.metadata.bind = "%s:///%s" % (settings.DATABASE_ENGINE, settings.DATABASE_NAME)
+    elixir.metadata.bind.echo = settings.SHOW_DEBUG_INFO
+    
+    models = import_user_module("models")
+    Entities = inspect_module(models, Entity)
+    setup(Entities)
+    
 
 @command
 def run(settings):
     
-    elixir.metadata.bind = "%s:///%s" % (settings.DATABASE_ENGINE, settings.DATABASE_NAME)
-    elixir.metadata.bind.echo = True
-    
-    models = import_user_module("models")
+    syncdb(settings)
     crawler = import_user_module("crawler")
-            
-    Spiders = inspect_module(crawler, BaseCrawler)    
-    Entities = inspect_module(models, Entity)    
-    UrlStorage = inspect_module(models, UrlEntity, get_first=True)
-        
-    setup(Entities)
+    models = import_user_module("models")
     
+    Spiders = inspect_module(crawler, BaseCrawler)    
+    UrlStorage = inspect_module(models, UrlEntity, get_first=True)
+                
     pool = GreenPool()    
     for Spider in Spiders:
     
@@ -37,6 +48,7 @@ def run(settings):
         pool.spawn_n(spider.start)
         
     pool.waitall()    
+
 
 
 
