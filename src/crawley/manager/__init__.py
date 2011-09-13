71da2f36df5a09936c3d960c4d2f363fee161cd5
Juan Manuel García <jmg.utn@gmail.com>
2011-09-13 09:50:16 -0300

    Project generator script and templates

--- src/crawley/manager/__init__.py
+++ src/crawley/manager/__init__.py
@@ -1,19 +1,30 @@
 import sys
 from commands import commands
+from generators import generators
 
 def run_cmd(settings, args):
         
     if len(args) <= 1:
         print "Subcommand not specified"
-    else:
-        cmd_name = args[1]
-        cmd_args = args[2:]
+        sys.exit(1)
+        
+    cmd_name = args[1]
+    cmd_args = args[2:]
+    
+    if settings is not None:
         cmd = commands.get(cmd_name)
-        if cmd is None:
-            print "[%s] Subcommand not valid" % (cmd_name)
-        else:
-            cmd(settings, *cmd_args)
-
+    else:
+        cmd = generators.get(cmd_name)
+        
+    if cmd is None:
+        print "[%s] Subcommand not valid" % (cmd_name)
+        sys.exit(1)
+    
+    if settings is not None:
+        cmd(settings, *cmd_args)
+    else:
+        cmd(*cmd_args)
+        
 
 def verify_settings(settings):
     
@@ -28,3 +39,8 @@ def manage(settings):
     settings = verify_settings(settings)
     sys.path.append(settings.PROJECT_ROOT)    
     run_cmd(settings, sys.argv)
+
+
+def execute():
+    
+    run_cmd(None, sys.argv)
---
+++ src/crawley/manager/templates/models.tm
@@ -0,0 +1,10 @@
+from crawley.persistance import Entity, UrlEntity, Field, Unicode
+
+class %(project_name)sUrls(UrlEntity):    
+    pass
+    
+class %(project_name)sClass(Entity):
+    
+    %(project_name)s_attribute = Field(Unicode(255))
+    
+    
