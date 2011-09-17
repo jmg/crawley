8e7719fd1fb05c65964c083452d8c6d91fc22a9a
David Litvak <dave@dave-netbook.(none)>
2011-09-17 11:26:48 -0300

    agregado tag parsing

--- crawley/parser/javaWorkspace/src/Parser.java
+++ crawley/parser/javaWorkspace/src/Parser.java
@@ -1,16 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Parser {
 
+	private Map<String, String> htmlElements = new HashMap<String, String>();
+	
+	public Parser() {
+		this.populateMaps();
+	}
+	
+	protected void populateMaps() {
+		htmlElements.put("'p'", "p");
+		htmlElements.put("'div'", "div");
+		htmlElements.put("'table'", "table");
+		htmlElements.put("'ul'", "ul");
+		htmlElements.put("'ol'", "ol");
+	}
+
 	public String parse(String crawleyDSL) {
-		String[] parsingString = crawleyDSL.split("=>");
-		String actionSection = parsingString[0].toLowerCase();
+		String[] parsingString = crawleyDSL.split(" => ");
+		String[] actionSection = parsingString[0].toLowerCase().split(" < ");
+		String action = actionSection[0];
+		String properties = actionSection[1];
 		//String getSection = parsingString[1];
 		
-		return "PyQuery(html).query('" + ((actionSection.contains("tag:'p'")) ? "p" : "div") + "')" + ((actionSection.contains("for")) ? "" : ((actionSection.contains("first")) ? "[0]" : "[-1]"));  
-		
-		//if (crawleyDSL.contains("'p'")) return "PyQuery(html).query('p')[0]";
-		//else if (crawleyDSL.contains("'div'")) return "PyQuery(html).query('div')[0]";
-		//return "";
+		return "return PyQuery(html).query('" + this.htmlElements.get(properties.split(":")[1]) + "')" + ((action.contains("all")) ? "" : ((action.contains("first")) ? "[0]" : "[-1]"));  
 	}
 
 }
