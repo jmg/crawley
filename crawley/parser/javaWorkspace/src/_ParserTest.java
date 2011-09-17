9b89a7cd3752c5c85a18404fe6232917b3a9210e
David Litvak <dave@dave-netbook.(none)>
2011-09-17 11:26:48 -0300

    agregado tag parsing

--- crawley/parser/javaWorkspace/src/_ParserTest.java
+++ crawley/parser/javaWorkspace/src/_ParserTest.java
@@ -7,13 +7,29 @@ import org.junit.*;
 public class _ParserTest {
 
 	@Test
-	public void lePasoUnaLineaDeDSLyMeRetornaAlgoParseado() {
-		assertEquals("pido el primer 'p' del html", "PyQuery(html).query('p')[0]", new Parser().parse("first : tag:'p' => innerHTML"));
-		assertEquals("pido el primer 'div' del html", "PyQuery(html).query('div')[0]", new Parser().parse("first : tag:'div' => innerHTML"));
-		assertEquals("pido el ultimo 'p' del html", "PyQuery(html).query('p')[-1]", new Parser().parse("last : tag:'p' => innerHTML"));
-		assertEquals("pido el ultimo 'div' del html", "PyQuery(html).query('div')[-1]", new Parser().parse("last : tag:'div' => innerHTML"));
-		assertEquals("pido todos los 'p' del html","PyQuery(html).query('p')", new Parser().parse("for : tag:'p' => innerHTML"));
-		assertEquals("pido todos los 'div' del html","PyQuery(html).query('div')", new Parser().parse("for : tag:'div' => innerHTML"));
+	public void pidoPrimerosElementosPorTagYRetornoElContenido() {
+		assertEquals("pido el primer 'p' del html", "return PyQuery(html).query('p')[0]", new Parser().parse("first < tag:'p' => innerHTML"));
+		assertEquals("pido el primer 'div' del html", "return PyQuery(html).query('div')[0]", new Parser().parse("first < tag:'div' => innerHTML"));
+		assertEquals("pido el primer 'table' del html", "return PyQuery(html).query('table')[0]", new Parser().parse("first < tag:'table' => innerHTML"));
+		assertEquals("pido el primer 'ol' del html", "return PyQuery(html).query('ol')[0]", new Parser().parse("first < tag:'ol' => innerHTML"));
+		assertEquals("pido el primer 'ul' del html", "return PyQuery(html).query('ul')[0]", new Parser().parse("first < tag:'ul' => innerHTML"));
 	}
 	
+	@Test
+	public void pidoUltimosElementosPorTagYRetornoElContenido() {
+		assertEquals("pido el ultimo 'p' del html", "return PyQuery(html).query('p')[-1]", new Parser().parse("last < tag:'p' => innerHTML"));
+		assertEquals("pido el ultimo 'div' del html", "return PyQuery(html).query('div')[-1]", new Parser().parse("last < tag:'div' => innerHTML"));
+		assertEquals("pido el ultimo 'table' del html", "return PyQuery(html).query('table')[-1]", new Parser().parse("last < tag:'table' => innerHTML"));
+		assertEquals("pido el ultimo 'ol' del html", "return PyQuery(html).query('ol')[-1]", new Parser().parse("last < tag:'ol' => innerHTML"));
+		assertEquals("pido el ultimo 'ul' del html", "return PyQuery(html).query('ul')[-1]", new Parser().parse("last < tag:'ul' => innerHTML"));
+	}
+	
+	@Test
+	public void pidoTodosLosElementosPorTagYRetornoElContenido() {
+		assertEquals("pido todos los 'p' del html","return PyQuery(html).query('p')", new Parser().parse("all < tag:'p' => innerHTML"));
+		assertEquals("pido todos los 'div' del html","return PyQuery(html).query('div')", new Parser().parse("all < tag:'div' => innerHTML"));
+		assertEquals("pido todos los 'table' del html", "return PyQuery(html).query('table')", new Parser().parse("all < tag:'table' => innerHTML"));
+		assertEquals("pido todos los 'ol' del html", "return PyQuery(html).query('ol')", new Parser().parse("all < tag:'ol' => innerHTML"));
+		assertEquals("pido todos los 'ul' del html", "return PyQuery(html).query('ul')", new Parser().parse("all < tag:'ul' => innerHTML"));
+	}
 }
