93af33432d42cb23cd57ea24174c129053d42c64
Juan Manuel García <jmg.utn@gmail.com>
2011-09-19 11:05:27 -0300

    verify settings refactorized

--- crawley/manager/commands/command.py
+++ crawley/manager/commands/command.py
@@ -1,4 +1,7 @@
-from utils import exit_with_error
+import sys
+import os 
+
+from utils import exit_with_error, import_user_module
 
 class BaseCommand(object):
     """
@@ -6,6 +9,7 @@ class BaseCommand(object):
     """
     
     name = "BaseCommand"
+    requires_settings = False
     
     def __init__(self, args):
         
@@ -15,6 +19,7 @@ class BaseCommand(object):
         """
             Checks for validations
         """
+        
         for validation, message in self.validations():
             if not validation:
                 exit_with_error(message)
@@ -24,17 +29,50 @@ class BaseCommand(object):
             Returns a list of tuples containing:
                 [(validate_condition, error_message)]
         """        
+        
         return []
         
     def execute(self):
         """
             Executes the command
         """
+        
         raise NotImplementedError()
 
     def checked_execute(self):
         """
             Checks before Execute
         """
+        
         self.check_validations()
+        if self.requires_settings:
+            self.settings = self._check_for_settings()
         self.execute()
+        
+    def _check_for_settings(self):
+        """
+            tries to import the user's settings file
+        """
+        
+        if len(self.args) > 0 and "--settings=" in self.args[0]:
+            settings_str = self.args[0].split("=")[1]
+            settings_dir, file_name = settings_str.rsplit("/")
+            sys.path.append(settings_dir)
+            settings_file = file_name[:-3]
+        else:
+            sys.path.append(os.getcwd())
+            settings_file = "settings"
+        
+        settings = import_user_module(settings_file)
+            
+        settings = self._check_setttings_errors(settings)
+        sys.path.append(settings.PROJECT_ROOT)
+        return settings
+            
+    def _check_setttings_errors(self, settings):
+        
+        if settings.DATABASE_ENGINE == 'sqlite':
+            if not settings.DATABASE_NAME.endswith(".sqlite"):
+                settings.DATABASE_NAME = "%s.sqlite" % settings.DATABASE_NAME 
+            
+        return settings
