f9e6d12a07304ef264490a963d386359c9d9ef3b
David Litvak <dave@dave-netbook.(none)>
2011-09-18 22:34:33 -0300

    cleaning up java code

--- crawley/parser/javaWorkspace/src/Parser.java
+++ crawley/parser/javaWorkspace/src/Parser.java
@@ -14,13 +14,20 @@ public class Parser {
 	}
 	
 	protected void populateMaps() {
-		actionElements.put("first", new FirstAction());
-		actionElements.put("last", new LastAction());
-		actionElements.put("all", new AllAction());
-		
+		populateActions();	
+		populateProperties();
+	}
+
+	protected void populateProperties() {
 		propertyElements.put("id", new IDProperty());
 		propertyElements.put("tag", new TagProperty());
 		propertyElements.put("class", new ClassProperty());
+	}
+
+	protected void populateActions() {
+		actionElements.put("first", new FirstAction());
+		actionElements.put("last", new LastAction());
+		actionElements.put("all", new AllAction());
 	}
 
 	public String parse(String crawleyDSL) {
@@ -30,10 +37,22 @@ public class Parser {
 		String properties = actionSection[1];
 		//String getSection = parsingString[1];
 		
-		return "return PyQuery(html).query('"
+		return getFinalQuery(action, properties);  
+	}
+
+	protected String getFinalQuery(String action, String properties) {
+		return "return " 
+				+ this.compoundProperty(properties, true)
+				+ "PyQuery(html).query('"
 				+ this.propertyElements.get(properties.split(":")[0]) 
 				+ this.trimSingleQuotes(properties.split(":")[1]) 
-				+ "')" + this.actionElements.get(action);  
+				+ "')" + this.actionElements.get(action)
+				+ this.compoundProperty(properties, false)
+				;
+	}
+
+	protected String compoundProperty(String properties, Boolean startingPosition) {
+		return (properties.contains("{") && properties.contains("}") ? ((startingPosition) ? "[" : "]") : ""); 
 	}
 
 	public String trimSingleQuotes(String htmlElement) {
