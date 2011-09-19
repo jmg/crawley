f8f15f99ca1d70c6c841257a701ec7422c12d550
David Litvak <dave@dave-netbook.(none)>
2011-09-18 23:38:29 -0300

    BNF Bugfix

--- crawley/parser/javaWorkspace/src/Parser.java
+++ crawley/parser/javaWorkspace/src/Parser.java
@@ -1,20 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 
 public class Parser {
 
-	private static final String ACTION_END = " < ";
+	private static final String CLOSING_PARENTHESIS = "')";
+	private static final String PYQUERY_HEAD = "PyQuery(html).query('";
+	private static final String RETURN = "return ";
+	private static final String ACTION_SEPARATOR = " < ";
 	private static final String QUERY_SEPARATOR = " => ";
 	private Map<String, Action> actionElements = new HashMap<String, Action>();
 	private Map<String, Property> propertyElements = new HashMap<String, Property>();
-	
+
 	public Parser() {
 		this.populateMaps();
 	}
-	
+
 	protected void populateMaps() {
-		populateActions();	
+		populateActions();
 		populateProperties();
 	}
 
@@ -32,27 +33,49 @@ public class Parser {
 
 	public String parse(String crawleyDSL) {
 		String[] parsingString = crawleyDSL.split(QUERY_SEPARATOR);
-		String[] actionSection = parsingString[0].toLowerCase().split(ACTION_END);
+		String[] actionSection = parsingString[0].toLowerCase().split(ACTION_SEPARATOR);
 		String action = actionSection[0];
 		String properties = actionSection[1];
-		//String getSection = parsingString[1];
-		
-		return getFinalQuery(action, properties);  
+		// String getSection = parsingString[1];
+
+		return getFinalQuery(action, properties);
 	}
 
 	protected String getFinalQuery(String action, String properties) {
-		return "return " 
-				+ this.compoundProperty(properties, true)
-				+ "PyQuery(html).query('"
-				+ this.propertyElements.get(properties.split(":")[0]) 
-				+ this.trimSingleQuotes(properties.split(":")[1]) 
-				+ "')" + this.actionElements.get(action)
-				+ this.compoundProperty(properties, false)
-				;
+		LinkedHashMap<String, String[]> propertyMap = new LinkedHashMap<String, String[]>();
+
+		for (String name : properties.split(" ")) propertyMap.put(name.split(":")[0], this.removeBraces(name.split(":")[1]).split(","));
+
+		String result = RETURN + this.compoundPropertyStartingBraces(properties);
+
+		for (String property : propertyMap.keySet()) {
+			for (int j = 0; j < propertyMap.get(property).length; j++)
+				result += ((j == 0) ? "" : ", ") + PYQUERY_HEAD
+						+ this.propertyElements.get(property)
+						+ this.trimSingleQuotes(propertyMap.get(property)[j])
+						+ CLOSING_PARENTHESIS + this.actionElements.get(action);
+		}
+
+		return result + this.compoundPropertyEndingBraces(properties);
 	}
 
-	protected String compoundProperty(String properties, Boolean startingPosition) {
-		return (properties.contains("{") && properties.contains("}") ? ((startingPosition) ? "[" : "]") : ""); 
+	protected String removeBraces(String propertyValues) {
+		return propertyValues.replace("{", "").replace("}", "");
+	}
+
+	protected String compoundPropertyEndingBraces(String properties) {
+		return this.compoundPropertyBraces(properties, false);
+	}
+
+	protected String compoundPropertyStartingBraces(String properties) {
+		return this.compoundPropertyBraces(properties, true);
+	}
+
+	protected String compoundPropertyBraces(String properties,
+			Boolean startingPosition) {
+		return (properties.contains("{") && properties.contains("}") ? 
+				((startingPosition) ? "[" : "]")
+				: "");
 	}
 
 	public String trimSingleQuotes(String htmlElement) {
--- crawley/parser/javaWorkspace/src/_ParserTest.java
+++ crawley/parser/javaWorkspace/src/_ParserTest.java
@@ -7,74 +7,57 @@ import org.junit.*;
 public class _ParserTest {
 
 	@Test
-	public void pidoPrimerosElementosPorTagYRetornoElContenido() {
+	public void primerosElementos() {
 		assertEquals("pido el primer 'p' del html", 
 				"return PyQuery(html).query('p')[0]", new Parser().parse("first < tag:'p' => innerHTML"));
-		assertEquals("pido el primer 'div' del html", 
-				"return PyQuery(html).query('div')[0]", new Parser().parse("first < tag:'div' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoUltimosElementosPorTagYRetornoElContenido() {
-		assertEquals("pido el ultimo 'p' del html", 
-				"return PyQuery(html).query('p')[-1]", new Parser().parse("last < tag:'p' => innerHTML"));
-		assertEquals("pido el ultimo 'div' del html", 
-				"return PyQuery(html).query('div')[-1]", new Parser().parse("last < tag:'div' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoTodosLosElementosPorTagYRetornoElContenido() {
-		assertEquals("pido todos los 'p' del html",
-				"return PyQuery(html).query('p')", new Parser().parse("all < tag:'p' => innerHTML"));
-		assertEquals("pido todos los 'div' del html",
-				"return PyQuery(html).query('div')", new Parser().parse("all < tag:'div' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoTodosElementoPorID() {
-		assertEquals("pido todos los elementos que tiene id = unid",
-				"return PyQuery(html).query('#unid')", new Parser().parse("all < id:'unID' => innerHTML"));
-		assertEquals("pido todos los elementos que tiene id = otroid",
-				"return PyQuery(html).query('#otroid')", new Parser().parse("all < id:'otroID' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoElPrimerElementoPorID() {
+		assertEquals("pido el primer elemento con tag = p o tag = div",
+				"return [PyQuery(html).query('p')[0], PyQuery(html).query('div')[0]]",new Parser().parse("first < tag:{'p','div'} => innerHTML"));
+		
 		assertEquals("pido el primer elemento que tiene id = unid",
 				"return PyQuery(html).query('#unid')[0]", new Parser().parse("first < id:'unID' => innerHTML"));
-		assertEquals("pido el primer elemento que tiene id = otroid",
-				"return PyQuery(html).query('#otroid')[0]", new Parser().parse("first < id:'otroID' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoElUltimoElementoPorID() {
-		assertEquals("pido el ultimo elemento que tiene id = unid",
-				"return PyQuery(html).query('#unid')[-1]", new Parser().parse("last < id:'unID' => innerHTML"));
-		assertEquals("pido el ultimo elemento que tiene id = otroid",
-				"return PyQuery(html).query('#otroid')[-1]", new Parser().parse("last < id:'otroID' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoTodosElementoPorClase() {
-		assertEquals("pido todos los elementos que tiene clase = mi-clase",
-				"return PyQuery(html).query('.mi-clase')", new Parser().parse("all < class:'mi-clase' => innerHTML"));
-	}
-	
-	@Test
-	public void pidoElPrimerElementoPorClase() {
+		assertEquals("pido el primer elemento con id = unid o id = otroid",
+				"return [PyQuery(html).query('#unid')[0], PyQuery(html).query('#otroid')[0]]",new Parser().parse("first < id:{'unid','otroid'} => innerHTML"));
+		
 		assertEquals("pido el primer elemento que tiene clase = mi-clase",
 				"return PyQuery(html).query('.mi-clase')[0]", new Parser().parse("first < class:'mi-clase' => innerHTML"));
+		assertEquals("pido el primer elemento con clase = mi-clase o clase = otra-clase",
+				"return [PyQuery(html).query('.mi-clase')[0], PyQuery(html).query('.otra-clase')[0]]",new Parser().parse("first < class:{'mi-clase','otra-clase'} => innerHTML"));		
 	}
 	
 	@Test
-	public void pidoElUltimoElementoPorClase() {
+	public void ultimosElementos() {
+		assertEquals("pido el ultimo 'p' del html", 
+				"return PyQuery(html).query('p')[-1]", new Parser().parse("last < tag:'p' => innerHTML"));
+		assertEquals("pido el ultimo elemento con tag = p o tag = div",
+				"return [PyQuery(html).query('p')[-1], PyQuery(html).query('div')[-1]]",new Parser().parse("last < tag:{'p','div'} => innerHTML"));
+
+		assertEquals("pido el ultimo elemento que tiene id = unid",
+				"return PyQuery(html).query('#unid')[-1]", new Parser().parse("last < id:'unID' => innerHTML"));
+		assertEquals("pido el ultimo elemento con id = unid o id = otroid",
+				"return [PyQuery(html).query('#unid')[-1], PyQuery(html).query('#otroid')[-1]]",new Parser().parse("last < id:{'unid','otroid'} => innerHTML"));
+
 		assertEquals("pido el ultimo elemento que tiene clase = mi-clase",
 				"return PyQuery(html).query('.mi-clase')[-1]", new Parser().parse("last < class:'mi-clase' => innerHTML"));
+		assertEquals("pido el ultimo elemento con clase = mi-clase o clase = otra-clase",
+				"return [PyQuery(html).query('.mi-clase')[-1], PyQuery(html).query('.otra-clase')[-1]]",new Parser().parse("last < class:{'mi-clase','otra-clase'} => innerHTML"));
 	}
 	
+	
 	@Test
-	public void pidoElementosPorDosClases() {
-		assertEquals("pido el primer elemento con clase = mi-clase o clase = otra-clase",
-				"return [PyQuery(html).query('.mi-clase')[0], PyQuery(html).query('.otra-clase')[0]]",new Parser().parse("first < class:{'mi-clase','otra-clase'} => innerHTML"));
+	public void todosLosElementos() {
+		assertEquals("pido todos los 'p' del html",
+				"return PyQuery(html).query('p')", new Parser().parse("all < tag:'p' => innerHTML"));
+		assertEquals("pido todos los elementos con tag = p o tag = div",
+				"return [PyQuery(html).query('p'), PyQuery(html).query('div')]",new Parser().parse("all < tag:{'p','div'} => innerHTML"));
+		
+		assertEquals("pido todos los elementos que tiene id = unid",
+				"return PyQuery(html).query('#unid')", new Parser().parse("all < id:'unID' => innerHTML"));
+		assertEquals("pido el ultimo elemento con id = unid o id = otroid",
+				"return [PyQuery(html).query('#unid'), PyQuery(html).query('#otroid')]",new Parser().parse("all < id:{'unid','otroid'} => innerHTML"));
+
+		assertEquals("pido todos los elementos que tiene clase = mi-clase",
+				"return PyQuery(html).query('.mi-clase')", new Parser().parse("all < class:'mi-clase' => innerHTML"));
+		assertEquals("pido todos los elementos con clase = mi-clase o clase = otra-clase",
+				"return [PyQuery(html).query('.mi-clase'), PyQuery(html).query('.otra-clase')]",new Parser().parse("all < class:{'mi-clase','otra-clase'} => innerHTML"));
 	}
 }
