91a503b2c8f99da9e569680dab025ae392a24656
Juan Manuel García <jmg.utn@gmail.com>
2011-09-19 09:48:19 -0300

    removed spurios files

--- zarasa/zarasa/models.py
+++
@@ -1,8 +0,0 @@
-from crawley.persistance import Entity, UrlEntity, Field, Unicode
-
-class zarasaUrls(UrlEntity):    
-    pass
-    
-class zarasaClass(Entity):
-    
-    zarasa_attribute = Field(Unicode(255))
