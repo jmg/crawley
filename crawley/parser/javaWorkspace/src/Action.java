6db59d709a3c42741b1a1070197e64daf1081c51
unknown <usuario@.(none)>
2011-09-20 12:47:01 -0300

    more java cleaning... beautifying the code

--- crawley/parser/javaWorkspace/src/Action.java
+++ crawley/parser/javaWorkspace/src/Action.java
@@ -3,16 +3,16 @@ import java.util.Map;
 
 
 public abstract class Action {
-	private static Map<String, Action> actionElements = new HashMap<String, Action>();
 	
-	protected static void populateActions() {
+	protected static Map<String, Action> populateActions() {
+		Map <String, Action> actionElements = new HashMap<String, Action>();
 		actionElements.put("first", new FirstAction());
 		actionElements.put("last", new LastAction());
 		actionElements.put("all", new AllAction());
+		return actionElements;
 	}
 	
 	public static Action getAction(String action) {
-		populateActions();
-		return actionElements.get(action);
+		return populateActions().get(action);
 	}
 }
