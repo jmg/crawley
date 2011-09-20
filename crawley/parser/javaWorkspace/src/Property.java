60e6d360baaaea0cd02e91a88318f9dd8d656e1f
David Litvak <kroma.harry@gmail.com>
2011-09-20 12:47:01 -0300

    more java cleaning... beautifying the code

--- crawley/parser/javaWorkspace/src/Property.java
+++ crawley/parser/javaWorkspace/src/Property.java
@@ -3,17 +3,16 @@ import java.util.Map;
 
 
 public abstract class Property {
-
-	private static Map<String, Property> propertyElements = new HashMap<String, Property>();
 	
-	protected static void populateProperties() {
+	protected static Map<String, Property> populateProperties() {
+		Map<String, Property> propertyElements = new HashMap<String, Property>();
 		propertyElements.put("id", new IDProperty());
 		propertyElements.put("tag", new TagProperty());
 		propertyElements.put("class", new ClassProperty());
+		return propertyElements;
 	}
 	
 	public static Property getProperty(String property) {
-		populateProperties();
-		return propertyElements.get(property);
+		return populateProperties().get(property);
 	}
 }
