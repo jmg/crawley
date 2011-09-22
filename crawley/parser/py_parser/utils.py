f17fd95ec681fc67d08255d10b5804f91b87ce1e
David Litvak <dave@dave-netbook.(none)>
2011-09-21 21:59:40 -0300

    cleaner code

--- crawley/parser/py_parser/utils.py
+++ crawley/parser/py_parser/utils.py
@@ -1,6 +1,7 @@
 """
     Parser utils
 """
+import re
 
 def remove_braces(property_values):
     
@@ -25,3 +26,12 @@ def compound_property_braces(properties, starting_position):
 def trim_single_quotes(html_element):
 
     return html_element.replace("'", "")
+
+def intersect(first_list, second_list):
+
+    return [x for x in first_list if x in second_list]
+
+def trim(a_string):
+    
+    pattern = re.compile(r'\s+')
+    return re.sub(pattern, ' ', a_string)
