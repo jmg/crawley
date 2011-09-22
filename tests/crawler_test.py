741a68405ea913ed292231d5f685727d5fcd3d27
Juan Manuel García <jmg.utn@gmail.com>
2011-09-22 10:47:36 -0300

    http cookies request

--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -1,18 +1,16 @@
 import unittest
 from crawley.crawlers import BaseCrawler
 
-from test_app.crawler import GoogleCrawler, GoogleScraper
-from test_app.models import GoogleText
 
 class CrawlerTest(unittest.TestCase):
     
     def test_requests(self):
         
         testCrawler = BaseCrawler()
-        response = testCrawler._get_response("http://www.facebook.com/login.php")
+        response = testCrawler._get_response("https://github.com/jmg")
         self.assertEqual(response.getcode(), 200)        
     
     def test_crawl(self):
         #TODO: make a test that crawl something
-        crawler = GoogleCrawler()
+        pass
         
