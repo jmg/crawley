9e134e09d17aacfb58b09d5fde0ebdc87e518308
David Litvak <dave@dave-netbook.(none)>
2011-09-24 14:34:27 -0300

    moved parsers parse to superclass Parse, generalized the parsing behaviour, just delegated some string generating stuff

--- crawley/parser/parsers/parser.py
+++ crawley/parser/parsers/parser.py
@@ -1,4 +1,6 @@
-from utils import trim
+from properties import Property
+from actions import Action
+import utils
 
 class Parser(object):
     
@@ -14,7 +16,21 @@ class Parser(object):
     
     def parse(self):
         
-        raise Exception("Abstract")
+        result = self.RETURN + self.COMPREHENSION_LIST_HEAD
+
+        count = 0
+        for key, value in self.dsl.properties.iteritems():
+            for index, property_element in enumerate(value):
+                result += ''.join([self._get_compound_positional_element(count),
+                          utils.not_first_element_plus(index), self.PYQUERY_HEAD, 
+                          Property().get(key),
+                          utils.trim_single_quotes(property_element),
+                          self.CLOSING_PARENTHESIS,
+                          Action().get(self.dsl.action)])
+            count += 1
+        
+        return "%s%s" % (result, self.COMPREHENSION_LIST_TAIL)
+
 
     def can_parse(self):
         
@@ -27,6 +43,10 @@ class Parser(object):
         raise Exception("Abstract")
     
     def _get_exception(self):
+        
+        raise Exception("Abstract")
+
+    def _get_compound_positional_element(self, count):
         
         raise Exception("Abstract")
 
--- crawley/parser/parsers/simple_parser.py
+++ crawley/parser/parsers/simple_parser.py
@@ -1,23 +1,10 @@
-import utils
 from parser import Parser, ParserException
-from properties import Property
-from actions import Action
 
 class SimpleParser(Parser):
     
-    def parse(self):
+    def _get_compound_positional_element(self, count):
         
-        result = self.RETURN + self.COMPREHENSION_LIST_HEAD
-
-        for key, value in self.dsl.properties.iteritems():
-            for index, property_element in enumerate(value):
-                result += ''.join([utils.not_first_element_plus(index), self.PYQUERY_HEAD, 
-                              Property().get(key),
-                              utils.trim_single_quotes(property_element),
-                              self.CLOSING_PARENTHESIS,
-                              Action().get(self.dsl.action)])
-
-        return "%s%s" % (result, self.COMPREHENSION_LIST_TAIL)
+        return ""
 
     def _can_parse(self):
         
