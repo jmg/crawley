b154f4f6bf1ffe1c82c8128c4ccd8d620b1700f1
David Litvak <dave@dave-netbook.(none)>
2011-09-24 14:34:27 -0300

    moved parsers parse to superclass Parse, generalized the parsing behaviour, just delegated some string generating stuff

--- crawley/parser/parsers/compound_parser.py
+++ crawley/parser/parsers/compound_parser.py
@@ -1,27 +1,6 @@
-import utils
 from parser import Parser, ParserException
-from properties import Property
-from actions import Action
 
 class CompoundParser(Parser):
-    
-    def parse(self):
-
-        
-        result = self.RETURN + self.COMPREHENSION_LIST_HEAD
-
-        count = 0
-        for key, value in self.dsl.properties.iteritems():
-            for index, property_element in enumerate(value):
-                result += ''.join([self._get_compound_positional_element(count),
-                          utils.not_first_element_plus(index), self.PYQUERY_HEAD, 
-                          Property().get(key),
-                          utils.trim_single_quotes(property_element),
-                          self.CLOSING_PARENTHESIS,
-                          Action().get(self.dsl.action)])
-            count += 1
-        
-        return "%s%s" % (result, self.COMPREHENSION_LIST_TAIL)
 
     def _get_compound_positional_element(self, count):
         
