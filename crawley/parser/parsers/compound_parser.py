3c8c755d1c7ac29b91790f40a0f095fed2082b93
David Litvak <dave@dave-netbook.(none)>
2011-09-24 18:49:45 -0300

    pushed down parse logic to implementing subclass

--- crawley/parser/parsers/compound_parser.py
+++ crawley/parser/parsers/compound_parser.py
@@ -1,6 +1,23 @@
 from parser import Parser, ParserException
+from properties import Property
+from actions import Action
+import utils
+
 
 class CompoundParser(Parser):
+
+    def _do_parse(self):
+        result = ""
+        
+        for count, (key, value) in enumerate(self.dsl.properties.iteritems()):
+            for index, property_element in enumerate(value):
+                result += ''.join([self._get_compound_positional_element(count),
+                          utils.not_first_element_plus(index), self.PYQUERY_HEAD,
+                          Property().get(key),
+                          utils.trim_single_quotes(property_element),
+                          self.CLOSING_PARENTHESIS,
+                          Action().get(self.dsl.action)])
+        return result
 
     def _get_compound_positional_element(self, count):
         
