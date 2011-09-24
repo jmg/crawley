d9f4628fef9382b3c814e9db8c563999dcf549a1
David Litvak <dave@dave-netbook.(none)>
2011-09-24 10:49:02 -0300

    otro parser ahora es el simple

---
+++ crawley/parser/parsers/crawley_dsl.py
@@ -0,0 +1,61 @@
+import utils
+
+class CrawleyDSL(object):
+    
+    def __init__(self, crawley_dsl):
+        
+        self.dsl = crawley_dsl
+        
+    def parse(self):
+        
+        for n, line in self._get_lines():
+            Line(line, n).parse()
+
+    def _get_lines(self):
+        
+        return enumerate(self.dsl.split("\n"))
+
+class Line(object):
+    
+    ACTION_SEPARATOR = " < "
+    QUERY_SEPARATOR = " => "
+    RECURSION_SEPARATOR = " -> "
+    TAG_PROPERTY_SEPARATOR = ":"
+    INTER_PROPERTY_SEPARATOR = ","
+        
+    def __init__(self, crawley_line, line_number):
+        
+        self.dsl = crawley_line
+        self.number = line_number
+
+        action_section, get_section = self.dsl.split(self.QUERY_SEPARATOR)
+        action, properties = action_section.lower().split(self.ACTION_SEPARATOR)
+
+        self.action = action
+
+        self.properties = self._get_properties(properties)
+
+    def is_simple(self):
+        
+        return len(self.properties) == 1 and not self.is_recursive()
+    
+    def is_compound(self):
+        
+        return not self.is_simple() and not self.is_recursive()
+    
+    def is_recursive(self):
+        
+        return self.dsl.count(self.RECURSION_SEPARATOR) > 0
+    
+    def parse(self):
+        
+        DSLAnalizer().parse(self)
+
+    def _get_properties(self, properties):
+
+        property_map = {}
+        for property_and_values in properties.split():
+            key, values = property_and_values.split(self.TAG_PROPERTY_SEPARATOR)
+            property_map[key] = utils.remove_braces(values).replace(" ", "").split(self.INTER_PROPERTY_SEPARATOR)
+        
+        return property_map
--- crawley/parser/parsers/parser.py
+++ crawley/parser/parsers/parser.py
@@ -5,12 +5,10 @@ class Parser(object):
     CLOSING_PARENTHESIS = "')"
     PYQUERY_HEAD = "PyQuery(html).query('"
     RETURN = "return "
-    ACTION_SEPARATOR = " < "
-    QUERY_SEPARATOR = " => "
 
-    def __init__(self, crawley_dsl):
+    def __init__(self, crawley_line):
         
-        self.dsl = trim(crawley_dsl)
+        self.dsl = crawley_line
     
     def parse(self):
         
@@ -19,49 +17,3 @@ class Parser(object):
     def can_parse(self):
         
         return "Abstract Error"
-    
-class CrawleyDSL(object):
-    
-    def __init__(self, crawley_dsl):
-        
-        self.dsl = crawley_dsl
-        
-    def parse(self):
-        for n, line in enumerate(dsl.split("\n")):
-            Line(line, n).parse()
-
-class Line(object):
-    
-    ACTION_SEPARATOR = " < "
-    QUERY_SEPARATOR = " => "
-        
-    def __init__(self, crawley_line, line_number):
-        self.dsl = crawley_line
-        self.number = line_number
-
-        action_section, get_section = self.dsl.split(self.QUERY_SEPARATOR)
-        action, properties = action_section.lower().split(self.ACTION_SEPARATOR)
-
-        self.action = action
-
-        self.property_map = {}
-
-        for property_and_values in properties.split():
-            key, values =  property_and_values.split(":")
-            property_map[key] = utils.remove_braces(values).split(",")
-
-    def is_simple(self):
-        
-        return len(self.property_map) == 1 and not self.is_recursive()
-    
-    def is_compound(self):
-        
-        return not self.is_simple() and not self.is_recursive()
-    
-    def is_recursive(self):
-        
-        return self.dsl.count("->") > 0
-    
-    def parse(self):
-        
-        DSLAnalizer().parse(self)
