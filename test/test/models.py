4f8a37a72dde450754249ec1661d1dd6458528ad
Juan Manuel García <jmg.utn@gmail.com>
2011-09-24 02:43:53 -0300

    removed spirous folder

--- test/test/models.py
+++
@@ -1,8 +0,0 @@
-from crawley.persistance import Entity, UrlEntity, Field, Unicode
-
-class testUrls(UrlEntity):    
-    pass
-    
-class testClass(Entity):
-    
-    test_attribute = Field(Unicode(255))
