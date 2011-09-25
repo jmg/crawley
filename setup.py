2373454f6b214a87261ff7caa4d58b553c8f8f4a
Juan Manuel García <jmg.utn@gmail.com>
2011-09-25 19:06:06 -0300

    Realeased pre-alpha version 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,13 @@ setup(
     author_email = "jmg.utn@gmail.com",
     license = "GPL v3",
     keywords = "Scraping Crawling Framework Python",    
-    packages=['crawley', 'crawley.manager', 'crawley.manager.commands', 'crawley.conf.project_template'],
+    packages=['crawley', 'crawley.manager', 'crawley.manager.commands', 'crawley.http', 'crawley.conf.project_template'],
     scripts=['crawley/bin/crawley'],
+    install_requires=[
+        'lxml',
+        'eventlet',
+        'elixir',
+        'pyquery',
+    ],
     url='https://github.com/jmg/crawley',
 )
