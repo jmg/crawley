246cb52196a691e3cfc8183b7d1e8cbb4a2b3682
Juan Manuel García <jmg.utn@gmail.com>
2011-10-02 04:30:13 -0300

    Refactored persistance

--- crawley/manager/commands/syncdb.py
+++ crawley/manager/commands/syncdb.py
@@ -1,5 +1,6 @@
 import elixir
 from crawley.persistance import Entity, setup
+from crawley.persistance.connectors import connectors
 
 from command import ProjectCommand
 from utils import inspect_module, import_user_module
@@ -15,8 +16,10 @@ class SyncDbCommand(ProjectCommand):
     name = "syncdb"
         
     def execute(self):
-                
-        elixir.metadata.bind = "%s:///%s" % (self.settings.DATABASE_ENGINE, self.settings.DATABASE_NAME)
+        
+        connector = connectors[self.settings.DATABASE_ENGINE](self.settings)
+        
+        elixir.metadata.bind = connector.get_connection_string()
         elixir.metadata.bind.echo = self.settings.SHOW_DEBUG_INFO
         
         models = import_user_module("models")
