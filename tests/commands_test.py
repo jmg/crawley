fe3fbff65f54aaffd062fcf1a948cc56c04e40cf
Juan Manuel García <jmg.utn@gmail.com>
2011-10-04 18:32:05 -0300

    first approach to xml documents generation with scraped data

--- tests/commands_test.py
+++ tests/commands_test.py
@@ -32,7 +32,7 @@ class CommandsTest(unittest.TestCase):
         self.assertTrue(os.path.exists("test_project.sqlite"))
         os.remove("test_project.sqlite")
         
-    def _test_run(self):
+    def test_run(self):
         
         cmd = RunCommand(self.settings_args)
         cmd.checked_execute()
