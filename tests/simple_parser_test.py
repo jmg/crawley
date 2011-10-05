b5bb006ee1f315bb99440eff8ad94793a089ace4
jm <jm@jm-desktop.(none)>
2011-10-05 18:02:23 -0300

    test generated scrapers added and a few modifications in DSL compiler.

--- tests/simple_parser_test.py
+++ tests/simple_parser_test.py
@@ -1,6 +1,8 @@
 import unittest
+from crawley.crawlers import BaseCrawler
 from crawley.simple_parser import interprete
 from crawley.simple_parser.parsers import SyntaxError
+from crawley.extractors import XPathExtractor
 
 class ParserTest(unittest.TestCase):
     
@@ -18,3 +20,12 @@ class ParserTest(unittest.TestCase):
         fail_dsl = """my_model = /html/body"""
         self.assertRaises(SyntaxError, interprete, fail_dsl)
             
+    def test_generated_scrapers(self):
+        
+        test_dsl = """my_model -> /html/body/div[5]/div/div/h1"""
+        Scraper = interprete(test_dsl)
+        
+        crawler = BaseCrawler()
+        html = crawler._get_data("http://www.python.org/")        
+                        
+        Scraper().scrape(XPathExtractor().get_object(html))
