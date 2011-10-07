80c0605196af34600698543f94230613c2aa4455
David Litvak <dave@dave-netbook.(none)>
2011-10-06 23:35:40 -0300

    some refactors

--- crawley/ia_generator/re_matcher.py
+++ crawley/ia_generator/re_matcher.py
@@ -1,11 +1,18 @@
 import re
 
+def _re_all_single_elem_match(html, elem):
+    """
+        Gets all elements of a single kind
+    """
+
+    return re.findall("<%s[\w|\s|=|\"]*>\s*([\w|\s|_|-]*):?\s*</%s>" % (elem,elem), html)
+
 def _re_title_match(html):
     """
         Gets all titles from a raw html
     """
 
-    return re.findall("<h[1-7]>([\w|\s|_|-]+):?</h[1-7]>", html)
+    return _re_all_single_elem_match(html, "h[1-7]")
 
 def get_title_matches(html, html2):
     """
@@ -14,12 +21,13 @@ def get_title_matches(html, html2):
 
     return len([x for x in _re_title_match(html) if x in _re_title_match(html2)]) / min(len(_re_title_match(html), len(_re_title_match(html2))
 
+
 def _re_td_match(html):
     """
         Gets all tds
     """
 
-    return re.findall("<td[\w|\s|=|\"]*>\s*([\w|\s|_|-]*):?\s*</td>",a)
+    return _re_all_single_elem_match(html, "td")
 
 def get_table_td_match(html, html2):
     """
@@ -33,7 +41,7 @@ def _re_th_match(html):
         Gets all th elements
     """
 
-    return re.findall("<th[\w|\s|=|\"]*>\s*([\w|\s|_|-]*):?\s*</th>",a)
+    return _re_all_single_elem_match(html, "th")
 
 def get_table_th_header_match(html, html2):
     """
