a47fe0a2f649ba6e25093c5134e751082adb8472
Juan Manuel García <jmg.utn@gmail.com>
2011-10-08 12:17:59 -0300

    simple_web_browser integration with crawley

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     author_email = "jmg.utn@gmail.com",
     license = "GPL v3",
     keywords = "Scraping Crawling Framework Python",    
-    packages=['crawley', 'crawley.manager', 'crawley.persistance', 'crawley.persistance.documents', 'crawley.manager.commands', 'crawley.http', 'crawley.conf.project_template'],
+    packages=['crawley', 'crawley.manager', 'crawley.persistance', 'crawley.persistance.documents', 'crawley.manager.commands', 'crawley.http', 'crawley.conf.project_template', 
+              'crawley.simple_web_browser', 'crawley.simple_web_browser.GUI'],
     scripts=['crawley/bin/crawley'],
     install_requires=[
         'lxml',
