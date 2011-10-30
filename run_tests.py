6af0b0797b8ac9453111986a67e04df1800ad664
David Litvak <david.litvakb@gmail.com>
2011-10-30 20:09:00 -0300

    removed some parameters from run_tests and made the existing really work =)

--- run_tests.py
+++ run_tests.py
@@ -12,22 +12,6 @@ from tests.commands_test import CommandsTest
 from tests.simple_parser_test import ParserTest
 from tests.persistance_test import PersistanceTest
 
-def _import_module(mod_name):
-    return __import__(mod_name, globals(), locals(), ["__all__"])
-
-def _clean_list(test_lists):
-    cleaned_list = []
-    for test_list in test_lists:    
-        for test in test_list:
-            cleaned_list.append(test)
-    return cleaned_list
-
-def get_all_tests():
-    modulo_tests = _import_module('tests')
-    all_tests_names = [_import_module('tests.%s' % module) for module in modulo_tests.__all__]
-    test_lists = [get_tests_for(test_name) for test_name in all_tests_names]        
-    return _clean_list(test_lists)
-
 def load_tests(tests):
     
     suite = unittest.TestSuite()
@@ -35,31 +19,25 @@ def load_tests(tests):
         tests = unittest.defaultTestLoader.loadTestsFromTestCase(test_class)
         suite.addTests(tests)
     return suite
-    
-def get_tests_for(modulo):
-    return ['%s.%s' % (modulo.__name__, m) for m in getattr(modulo, '__all__', [])]
-    
+      
 def suite(options):
+    unit = [UtilsTest, ParserTest, PersistanceTest]
+    integration = [CommandsTest, CrawlerTest]
     
     if options.all is not None:
-        tests = get_all_tests()
-    elif options.module is not None:
-        modulo = _import_module("tests.%s" % options.module)
-        tests = get_tests_for(modulo)
+        return load_tests(unit + integration)
     elif options.unittests is not None:
-        return load_tests([UtilsTest, ParserTest, PersistanceTest])
+        return load_tests(unit)
     elif options.integration is not None:
-        return load_tests([CommandsTest, CrawlerTest])
+        return load_tests(integration)
     else:   
         return None
-    return unittest.defaultTestLoader.loadTestsFromNames(tests)
-
+    
 if __name__ == "__main__":
     
     parser = OptionParser()
     parser.add_option("-i", "--integration", help="run integration tests", nargs=0)
     parser.add_option("-u", "--unittests", help="run unit tests", nargs=0)
-    parser.add_option("-m", "--module", help="run tests from a specific module")
     parser.add_option("-a", "--all", help="run all tests", nargs=0)
     (options, args) = parser.parse_args()
     test_suite = suite(options)
