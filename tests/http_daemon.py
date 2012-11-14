6fb5d4e62db07085f766da419961b4611920a297
pedro <pedro@laptop.(none)>
2012-11-14 14:44:35 -0300

    Adding unittest.

---
+++ tests/http_daemon.py
@@ -0,0 +1,41 @@
+import SimpleHTTPServer
+import SocketServer
+import StringIO
+
+PORT = 8000
+
+class LoggedRequest(object):
+    def __init__(self, command, path, request_version, headers, request_body):
+        self.command = command
+        self.path = path
+        self.request_version = request_version
+        self.headers = dict(headers)
+        self.request_body = request_body 
+
+    def __repr__(self):
+        return '<LoggedRequest %s %s %s>' % (self.command, self.path,
+                                             self.request_version)
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    requests = []
+    
+    def do_GET(self):
+        """Serve a GET request."""
+        self.wfile.write('ABCDEF\n')
+                
+    def handle_one_request(self):
+        # TODO: Add support for reading self.rfile
+        request_body = None
+        
+        SimpleHTTPServer.SimpleHTTPRequestHandler.handle_one_request(self)
+        self.requests.append(LoggedRequest(self.command, self.path,
+                                           self.request_version, self.headers,
+                                           request_body))
+        
+
+if __name__ == '__main__':
+    httpd = SocketServer.TCPServer(("127.0.0.1", PORT), ServerHandler)
+    
+    print "Serving at port", PORT
+    httpd.serve_forever()
